c9a75ecaae850d35302233e5459ba768ebe4bd21
Daniel Serrano Rodríguez <a_daniel.serrano.rodriguez@iespablpicasso.es>
2023-05-15 19:57:31 +0200

    Top bar visual "hecha"

--- src/grabberApp/javafx/fxmls/ControllerTopBar.java
+++ src/grabberApp/javafx/fxmls/ControllerTopBar.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import models.AbstractController;
@@ -18,7 +19,22 @@ public class ControllerTopBar extends AbstractController {
 	private ImageView imgHome;
 
 	@FXML
+	private ImageView imgSwitchType;
+
+	@FXML
+	private ImageView imgSearch;
+
+	@FXML
 	private Button btnHome;
+
+	@FXML
+	private Button btnSwitchType;
+
+	@FXML
+	private Button btnSearch;
+
+	@FXML
+	private TextField txfSearch;
 
 	public ControllerTopBar() {
 		gApp = Utils.gApp;
@@ -35,6 +51,26 @@ public class ControllerTopBar extends AbstractController {
 		}
 
 		imgHome.setImage(new Image(fis));
+
+		file = new File(gApp.getClass().getResource("/img/icon/play.png").getPath());
+
+		try {
+			fis = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		imgSwitchType.setImage(new Image(fis));
+
+		file = new File(gApp.getClass().getResource("/img/icon/search.png").getPath());
+
+		try {
+			fis = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		imgSearch.setImage(new Image(fis));
 	}
 
 	@FXML
